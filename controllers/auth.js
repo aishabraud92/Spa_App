09afaf639647da970eaf7b36f25bf2db7614efca
aishabraud92 <aishabraud@gmail.com>
2018-01-23 20:17:46 -0800

    auth set up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/signup', function(req, res)){
+router.get('/signup', function(req, res) {
   res.render('auth/signup');
-
 });
 
-router.get('/login', function(req, res){
+router.get('/login', function(req, res) {
   res.render('auth/login');
 });
 
