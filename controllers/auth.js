21cd2b87e0caf40a3221a2aa25272acd2a255148
aishabraud92 <aishabraud@gmail.com>
2018-01-23 19:56:35 -0800

    initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/signup', function(req, res)){
+  res.render('auth/signup');
+
+});
+
+router.get('/login', function(req, res){
+  res.render('auth/login');
+});
+
+module.exports = router;
